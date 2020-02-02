e9e95b0125655f35ad79528237f22e8d3d8d9fdb
Andrey Trusov <trusov@haulmont.com>
2020-02-02 13:36:17 +0400

    init

--- src/ru/papont/guziv/part1/Main.java
+++ src/ru/papont/guziv/part1/Main.java
@@ -2,6 +2,28 @@ package ru.papont.guziv.part1;
 
 public class Main {
     public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        System.out.println(Thread.currentThread().getName());
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread2 = new Thread(myRunnable);
+        thread2.start();
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
 
     }
 }
