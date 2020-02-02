59947a4971fa9c655b15f5c782ef878277a80f4f
Andrey Trusov <trusov@haulmont.com>
2020-02-02 13:41:51 +0400

    end part1

--- src/ru/papont/guziv/part1/Main.java
+++ src/ru/papont/guziv/part1/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         myThread.start();
+//        myThread.run();
 
         System.out.println(Thread.currentThread().getName());
 
@@ -16,7 +17,11 @@ public class Main {
 class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 400; i++) {
+            System.out.println("Thread name is - " + Thread.currentThread().getName() + " i = " + i);
+
+        }
+        throw new RuntimeException();
     }
 }
 
