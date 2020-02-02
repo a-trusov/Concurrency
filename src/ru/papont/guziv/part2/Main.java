46e895d2ba2d8e02adaf086c2a14d49a9cfc4431
Andrey Trusov <trusov@haulmont.com>
2020-02-02 14:17:36 +0400

    part2 1

--- src/ru/papont/guziv/part2/Main.java
+++ src/ru/papont/guziv/part2/Main.java
@@ -1,4 +1,18 @@
 package ru.papont.guziv.part2;
 
 public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        Thread.sleep(1000);
+        System.out.println("thread main");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("thread 0");
+    }
 }
